f1e9c779ec8260be9e08ebe7c46f771cffa82227
Alexander Curtis <alexcu@tutanota.com>
2025-05-27 18:16:16 +0100

    fixed incorrect use of stat when should be lstat, i dont traverse symbolic links

--- src/traits_and_conversions.rs
+++ src/traits_and_conversions.rs
@@ -1,64 +1,87 @@
+use crate::{LOCAL_PATH_MAX,Result};
+use std::ffi::OsStr;
+use std::os::unix::ffi::OsStrExt;
+use std::mem::MaybeUninit;
+use std::path::Path;
+use libc::{lstat,stat};
+
 pub trait BytesToCstrPointer {
-    fn as_cstr_ptr<F, R>(&self, f: F) -> R
+    fn as_cstr_ptr<T,F, R>(&self, f: F) -> R
     where
-        F: FnOnce(*const libc::c_char) -> R;
+        F: FnOnce(*const T) -> R;
 }
 //convenience thing for me suck it
 pub trait ToOsStr {
-    fn to_os_str(&self) -> &std::ffi::OsStr;
+    fn to_os_str(&self) -> &OsStr;
 }
 
 impl ToOsStr for [u8] {
-    fn to_os_str(&self) -> &std::ffi::OsStr {
-        std::os::unix::ffi::OsStrExt::from_bytes(self)
+    fn to_os_str(&self) -> &OsStr {
+        OsStrExt::from_bytes(self)
     }
 }
 
 impl BytesToCstrPointer for [u8] {
     #[inline]
-    ///converts a byte slice into a c str(ing) pointer
-    ///utilises `LOCAL_PATH_MAX` converts a file of up to 512 birts to create an upper bounded array
-    //needs to be done as a callback because we need to keep the reference to the array
-    //apparently this can fuck up on some weird filesystems, like NTFS(`PATH_MAX` ) being incorrect.
-    //needs to be done as a callback because we need to keep the reference to the array
-    //apparently this can fuck up on some weird filesystems, like NTFS(`PATH_MAX` ) being incorrect.
-    fn as_cstr_ptr<F, R>(&self, f: F) -> R
+    fn as_cstr_ptr<T,F, R>(&self, f: F) -> R
     where
-        F: FnOnce(*const i8) -> R,
+        F: FnOnce(*const T) -> R,
     {
-        let mut c_path_buf = [0u8; crate::LOCAL_PATH_MAX];
-        c_path_buf[..self.len()].copy_from_slice(self);
-        // null terminate the string
-        c_path_buf[self.len()] = 0;
-        f(std::ptr::addr_of!(c_path_buf).cast::<i8>())
-        //if you thought nested macros were  a pain then hello
+        debug_assert!(
+            self.len() < LOCAL_PATH_MAX,
+            "Input too large for buffer"
+        );
+
+        // //initialise  the buffer (this doesn't actually create the memory)
+        let mut c_path_buf = MaybeUninit::<[u8; LOCAL_PATH_MAX]>::uninit();
+
+        unsafe {
+            // Get a raw pointer to the buffer's data
+            let buf_ptr = c_path_buf.as_mut_ptr() as *mut u8;
+            
+            // Copy the slice into the buffer
+            std::ptr::copy_nonoverlapping(
+                self.as_ptr(),
+                buf_ptr,
+                self.len(),
+            );
+            
+            // Add null terminator
+            buf_ptr.add(self.len()).write(0);
+            
+            // Call the callback with the pointer to the null-terminated string
+            f(buf_ptr as *const T)
+        }
     }
 }
+
+
+
 
 pub trait PathToBytes {
     fn to_bytes(&self) -> &[u8];
 }
 
-impl PathToBytes for std::path::Path {
+impl PathToBytes for Path {
     #[inline]
     fn to_bytes(&self) -> &[u8] {
-        std::os::unix::ffi::OsStrExt::as_bytes(self.as_os_str())
+        OsStrExt::as_bytes(self.as_os_str())
     }
 }
 
 pub trait ToStat {
     #[allow(clippy::missing_errors_doc)] //SKIPPING ERRORS UNTIL DONE.
-    fn get_stat(&self) -> crate::Result<libc::stat>;
+    fn get_stat(&self) -> Result<stat>;
 }
 
 impl ToStat for crate::DirEntry {
     ///Converts into `libc::stat` , mostly for internal use..probably...
     #[inline]
-    fn get_stat(&self) -> crate::Result<libc::stat> {
-        let mut stat_buf = std::mem::MaybeUninit::<libc::stat>::uninit();
+    fn get_stat(&self) -> Result<stat> {
+        let mut stat_buf = MaybeUninit::<stat>::uninit();
 
         let res = self
-            .as_cstr_ptr(|ptr| unsafe { libc::stat(ptr, std::ptr::addr_of_mut!(stat_buf).cast()) });
+            .as_cstr_ptr(|ptr| unsafe { lstat(ptr, stat_buf.as_mut_ptr()) });
 
         if res == 0 {
             Ok(unsafe { stat_buf.assume_init() })
@@ -71,10 +94,10 @@ impl ToStat for crate::DirEntry {
 impl ToStat for &[u8] {
     #[inline]
     ///Converts a byte slice into  `libc::stat`, internal probably.
-    fn get_stat(&self) -> crate::Result<libc::stat> {
-        let mut stat_buf = std::mem::MaybeUninit::<libc::stat>::uninit();
+    fn get_stat(&self) -> Result<stat> {
+        let mut stat_buf = MaybeUninit::<stat>::uninit();
         let res = self
-            .as_cstr_ptr(|ptr| unsafe { libc::stat(ptr, std::ptr::addr_of_mut!(stat_buf).cast()) });
+            .as_cstr_ptr(|ptr| unsafe { lstat(ptr, stat_buf.as_mut_ptr()) });
 
         if res == 0 {
             Ok(unsafe { stat_buf.assume_init() })

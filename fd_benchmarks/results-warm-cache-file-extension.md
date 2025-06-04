40e3cd1207e80ccb0be9dbcb0525f46088b82ee1
Alexander Curtis <alexcu@tutanota.com>
2025-06-04 23:12:03 +0100

    found a discrepenacy on extension check, discrepancy documented and explain in benchmarks code

--- fd_benchmarks/results-warm-cache-file-extension.md
+++ fd_benchmarks/results-warm-cache-file-extension.md
@@ -1,4 +1,4 @@
 | Command | Mean [ms] | Min [ms] | Max [ms] | Relative |
 |:---|---:|---:|---:|---:|
-| `fdf -HI --extension 'jpg' '' '/home/alexc'` | 224.0 ± 2.9 | 219.8 | 228.4 | 1.00 |
-| `fd -HI --extension 'jpg' '' '/home/alexc'` | 376.3 ± 3.9 | 372.4 | 385.1 | 1.68 ± 0.03 |
+| `fdf -HI --extension 'jpg' '' '/home/alexc'` | 292.6 ± 2.0 | 289.5 | 295.8 | 1.00 |
+| `fd -HI --extension 'jpg' '' '/home/alexc'` | 516.3 ± 5.8 | 509.1 | 524.1 | 1.76 ± 0.02 |
--- fd_benchmarks/warm-cache-file-extension.sh
+++ fd_benchmarks/warm-cache-file-extension.sh
@@ -15,4 +15,12 @@ hyperfine --warmup "$WARMUP_COUNT" \
     "$COMMAND_FD" \
     --export-markdown results-warm-cache-file-extension.md
 
-check_for_differences "false" "$COMMAND_FIND" "$COMMAND_FD"
+### my extension finder works different to fd, basically i found out#
+### my detector doesnt find  eg /home/alexc/.cache/paru/clone/electron20/src/src/third_party/ffmpeg/tests/ref/lavf/jpg
+##this is because it doesnt have a ., mine specifically looks for a dot, which how an extension
+##SHOULD be defined, whatever
+
+
+check_for_differences "true" "$COMMAND_FIND" "$COMMAND_FD"
+echo  "the difference between the results is shown below, it should be 1 file, an edgecase i dont know how to fix AHHH"
+echo $(( $(diff /tmp/results.fd /tmp/results.find | wc -l) / 2 ))

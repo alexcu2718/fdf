5c5a9f6324da2bd85123c6b70bce89128486f3b4
Alexander Curtis <alexcu@tutanota.com>
2025-06-10 08:50:38 +0100

    improved type filter benchmark

--- fd_benchmarks/warm-cache-type-filtering.sh
+++ fd_benchmarks/warm-cache-type-filtering.sh
@@ -2,13 +2,28 @@
 
 source "prelude.sh"
 
-COMMAND_FIND="fdf .  '$SEARCH_ROOT' -HI --type l"
-COMMAND_FD="fd -HI '' '$SEARCH_ROOT' --type l"
+
+source "new_prelude.sh"
+
+COMMAND_FIND_SL="fdf .  '$SEARCH_ROOT' -HI --type l"
+COMMAND_FD_SL="fd -HI '' '$SEARCH_ROOT' --type l"
 
 hyperfine --warmup "$WARMUP_COUNT" \
-    "$COMMAND_FIND" \
-    "$COMMAND_FD" \
+    "$COMMAND_FIND_SL" \
+    "$COMMAND_FD_SL" \
     --export-markdown results-warm-cache-type-filtering.md
 
-check_for_differences "false" "$COMMAND_FIND" "$COMMAND_FD"
+check_for_differences "false" "$COMMAND_FIND_SL" "$COMMAND_FD_SL"
+
+
+
+COMMAND_FIND_EMPTY="fdf .  '$SEARCH_ROOT' -HI --type e"
+COMMAND_FD_EMPTY="fd -HI '' '$SEARCH_ROOT' --type e"
+
+hyperfine --warmup "$WARMUP_COUNT" \
+     "$COMMAND_FIND_EMPTY" \
+     "$COMMAND_FD_EMPTY" \
+     --export-markdown results-warm-cache-type-filtering.md
+
+check_for_differences "false" "$COMMAND_FIND_EMPTY" "$COMMAND_FD_EMPTY"
 

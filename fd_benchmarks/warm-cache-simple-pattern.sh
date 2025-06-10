dcb120b1f252dddda2b61a73fc6abc4224814d68
Alexander Curtis <alexcu@tutanota.com>
2025-06-10 17:55:46 +0100

    changed simple pattern a bit

--- fd_benchmarks/warm-cache-simple-pattern.sh
+++ fd_benchmarks/warm-cache-simple-pattern.sh
@@ -3,17 +3,14 @@
 source "prelude.sh"
 
 source "new_prelude.sh"
-pattern="\.md$"
+pattern="'.*[0-9].*(md|\.c)$'"
+
 COMMAND_FIND="fdf -HI $pattern '$SEARCH_ROOT'"
-COMMAND_FIND2="fdf  $pattern '$SEARCH_ROOT'"
 COMMAND_FD="fd -HI $pattern '$SEARCH_ROOT'"
-COMMAND_FD2="fd  $pattern  '$SEARCH_ROOT'"
 
 hyperfine --warmup "$WARMUP_COUNT" \
     "$COMMAND_FIND" \
-    "$COMMAND_FIND2" \
     "$COMMAND_FD" \
-    "$COMMAND_FD2" \
     --export-markdown results-warm-cache-simple-pattern.md
 
 check_for_differences "false" "$COMMAND_FIND" "$COMMAND_FD"

44ac2dba847c4f701e4470732c0e6e3dc6122492
Alexander Curtis <alexcu@tutanota.com>
2025-07-12 19:02:38 +0100

    added some tests, fixed some UB (miri is not straightforward with parallelism...)

--- src/lib.rs
+++ src/lib.rs
@@ -112,10 +112,10 @@ mod filetype;
 pub use filetype::FileType;
 
 //this allocator is more efficient than jemalloc through my testing(still better than system allocator)
-//#[cfg(any(target_os = "linux", target_os = "macos"))]
+#[cfg(any(target_os = "linux", target_os = "macos"))]
 //not sure which platforms support this, BSD doesnt from testing, will test others as appropriate(GREAT DOCS!!!)
-//#[global_allocator]
-//static ALLOC: mimalloc::MiMalloc = mimalloc::MiMalloc;
+#[global_allocator]
+static ALLOC: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
 #[derive(Debug)]
 /// A struct to find files in a directory.

76380429dbb9bd470fcbba4571d7d4cfaa79322c
Alexander Curtis <alexcu@tutanota.com>
2025-07-24 17:20:42 +0100

    cleanup (unnecessary code)

--- build.rs
+++ build.rs
@@ -10,9 +10,7 @@ fn main() {
     let num_threads =
         thread::available_parallelism().map_or(MIN_THREADS, core::num::NonZeroUsize::get);
 
-    if num_threads == MIN_THREADS {
-        println!("cargo:rustc-env=CPU_COUNT={MIN_THREADS}");
-    } else {
+  
         println!("cargo:rustc-env=CPU_COUNT={num_threads}");
-    }
+    
 }

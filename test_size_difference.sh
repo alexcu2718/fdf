4c949bee25d2aa52faea29723202ddebb7635d54
Alexander Curtis <alexcu@tutanota.com>
2025-08-24 20:55:00 +0100

    added an example to show a 1 file diff, quite odd

---
+++ test_size_difference.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo 'this script is an example of a disparity in size filtering i'm seeing, it is extremely odd!, is it me, or fd who's wrong?'
+( fd . /tmp/llvm-project -HI --size -500b ) | sort > fd.out
+echo "fd output count is  $(cat fd.out | wc -l ) "
+( fdf . /tmp/llvm-project -HI --size -500b ) | sort > fdf.out
+echo "fdf output count is $(cat fdf.out | wc -l ) "
+echo "there is a disparity of 1 file in fdf that isnt in fd"
+diff fdf.out fd.out | grep '^<' | cut -d' ' -f2-

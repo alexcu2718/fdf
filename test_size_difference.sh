2fade27a90d5dca3652ddf220c17fd1175df7e1b
Alexander Curtis <alexcu@tutanota.com>
2025-09-01 12:39:01 +0100

    update

--- test_size_difference.sh
+++ test_size_difference.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+( fdf . /tmp/llvm-project -HI --size -500b ) | sort > fdf.out
+( fd . /tmp/llvm-project -HI --size -500b ) | sort > fd.out
 echo "this script is an example of a disparity in size filtering i'm seeing, quite nebulous"
 echo "fd output count is  $(cat fd.out | wc -l ) "
-( fdf . /tmp/llvm-project -HI --size -500b ) | sort > fdf.out
 echo "fdf output count is $(cat fdf.out | wc -l ) "
 echo "there is a disparity of 1 file in fdf that isnt in fd"
 diff fdf.out fd.out | grep '^<' | cut -d' ' -f2-

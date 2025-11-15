3c4e02845cb4d82e467062cacd0c2d940191fbb9
Alexander Curtis <alexcu@tutanota.com>
2025-11-15 15:43:30 +0000

    compatibility: shell scripts for compat work

--- research/generate_dirents.sh
+++ research/generate_dirents.sh
@@ -1,31 +1,31 @@
 #!/usr/bin/env bash
 
-##this is just a simple struct for inspecting all the dirent structs in libc
-## naturally, libc is located in my home folder, I didn't want to include it in this repo.
 
 cd "$(dirname $0)"
 
+
+WORD_SIZE=$(getconf LONG_BIT)
+
 SETCD="$PWD"
 
-cd ~/libc && git pull  > /dev/null
+LIBC_LOCATION="../libc"
+
+
+if [ ! -d $LIBC_LOCATION ]; then
+    git clone --depth 1 https://github.com/rust-lang/libc.git $LIBC_LOCATION
+fi
+
+[ -d $LIBC_LOCATION ] || { echo "$LIBC_LOCATION not found investigate this!"; exit 1; }
+
+cd $LIBC_LOCATION && git pull  > /dev/null 2>&1 
 
 cd $SETCD
 
-[ -d ~/libc ] || { echo "~/libc not found"; exit 1; }
 
-
-
-rg 'pub struct dirent' -n ~/libc | cut -d: -f1 | sort -u | while read -r file; do
-  echo "### $file"
-  awk '/pub struct dirent/ {found=1} found {print} /\}/ && found {found=0}' "$file"
-  echo
-done > dirent_structs.txt
-
-rg 'type ino' -n ~/libc | cut -d: -f1 | sort -u | while read -r file; do
-  echo "### $file"
-  awk '/type ino/ {found=1} found {print} /;/ && found {found=0}' "$file"
-  echo
-done >> dirent_structs.txt
-
+rg 'pub struct dirent' -n $LIBC_LOCATION | cut -d: -f1 | sort -u | while read -r file; do
+  echo "### $file" && awk '/pub struct dirent/ {found=1} found {print} /\}/ && found {found=0}' "$file" && echo
+  ino_file=$(grep -l "type ino" "$file" 2>/dev/null || find $(dirname "$file") -name "*.rs" -exec grep -l "type ino" {} \; | head -1)
+  [ -n "$ino_file" ] && echo "### $ino_file" && awk '/type ino/ {found=1} found {print} /;/ && found {found=0}' "$ino_file" && echo
+done  > /dev/null 2>&1 > dirent_structs.txt
 
 cat dirent_structs.txt

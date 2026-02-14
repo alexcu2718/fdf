9b4bd79991c1061238881e05e6e97923a8700c4d
Alexander Curtis <alexcu@tutanota.com>
2026-02-14 23:42:58 +0000

    feat: add getdirentries for freebsd

--- src/fs/dir_entry.rs
+++ src/fs/dir_entry.rs
@@ -1680,18 +1680,17 @@ impl DirEntry {
     }
 
     /**
-    Low-level directory iterator using macOS `getdirentries` API.
+    Low-level directory iterator using macOS/FreeBSD `getdirentries64`/`GetDirEntries` API.
 
-    This method provides a macOS-specific, high-performance streaming iterator
+    This method provides a macOS/BSD-specific, high-performance streaming iterator
     over directory entries by leveraging the platform's `getdirentries(2)`
-    family. It is analogous to the Linux `getdents` specialisation but
-    implemented for BSD-derived macOS interfaces and conventions.
+    family. It is analogous to the Linux `getdents` specialisation
 
-    It implements a specialised EOF trick to avoid an extra syscall to terminate reading
+    It implements a specialised EOF trick to avoid an extra syscall to terminate reading on macOS
 
 
     # Platform
-    - macOS only
+    - macOS only/FreeBSD only
 
     # Errors
     Returns `Err` when the directory cannot be opened or read, when the
